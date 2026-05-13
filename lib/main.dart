00a581efb522004d8e6effbc9c3d43bc93cffddf
joao.teodoro <joao.teodoro1610@gmail.com>
2026-05-13 16:33:46 -0300

    feat: Integrate Firebase services with Crashlytics and configure project settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
+import 'dart:ui';
+
 import 'package:chefia_app/app/chefia_app.dart';
+import 'package:chefia_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(const ChefiaApp());
 }
